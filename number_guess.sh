9a84c05f618bae6d633bfb741c57feacc752f481
Seohyeon Lee <seohyeonlee2020@gmail.com>
2024-11-22 16:00:34 +0000

    feat: program is working

--- number_guess.sh
+++ number_guess.sh
@@ -9,7 +9,7 @@ if [[ ! -z $USER_RESULT ]]
 then
 #get data
 USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
-GAMES_PLAYED=$($PSQL "SELECT games_played FROM users WHERE username='$USERNAME'")
+GAMES_PLAYED=$($PSQL "SELECT count(*) FROM games WHERE user_id=$USER_ID")
 BEST_GAME=$($PSQL "SELECT MIN(number_of_guesses) FROM games WHERE user_id=$USER_ID")
 #print welcome
 echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
@@ -21,15 +21,15 @@ fi
 #save user id
 USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
 
-#generate random num
 RANDOM_NUM=$(($RANDOM%1000+1))
 #echo $RANDOM_NUM
 NUM_TRIES=1
-echo -e "\nGuess the secret number between 1 and 1000:"
+echo -e "Guess the secret number between 1 and 1000:"
 read GUESS
 #if guess is not an integer loop until you get a valid guess
 while [[ ! $GUESS =~ ^[0-9]+$ ]]
 do
+(( NUM_TRIES++ ))
 echo -e "\nThat is not an integer, guess again:"
 read GUESS
 done
@@ -37,7 +37,7 @@ done
 while [[ $GUESS != $RANDOM_NUM ]]
 do
 (( NUM_TRIES++ ))
-if [[ $GUESS 65 $RANDOM_NUM ]]
+if [[ $GUESS -lt $RANDOM_NUM ]]
   then
   echo -e "\nIt's higher than that, guess again:"
   read GUESS
@@ -50,7 +50,7 @@ done
 #add game results to database
 GAME_RESULT=$($PSQL "INSERT INTO games(user_id, number_of_guesses) VALUES($USER_ID, $NUM_TRIES)")
 
-GAMES_PLAYED=(( GAMES_PLAYED++ ))
+(( GAMES_PLAYED++ ))
 #update user data
 UPDATE_NUM_GAMES=$($PSQL "UPDATE users SET games_played=$GAMES_PLAYED WHERE user_id=$USER_ID")
 BEST_GAME=$($PSQL "select min(number_of_guesses) from games where user_id=$USER_ID")
